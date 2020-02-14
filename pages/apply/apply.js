056c08014dcbfb6f4de638bfa0277de16b88173b
hanyongbin <hanyongbin@souche.com>
2020-02-14 18:46:03 +0800

    首页，开通页

---
+++ pages/apply/apply.js
@@ -0,0 +1,91 @@
+// pages/apply/apply.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        text1: '返利比例',
+        text2: '返利最高5%'
+      },
+      {
+        text1: '结算时间',
+        text2: '次月的10号%'
+      },
+      {
+        text1: '投入成本',
+        text2: '0成本投入'
+      },
+      {
+        text1: '风险评估',
+        text2: '无任何风险'
+      },
+      {
+        text1: '收入分析',
+        text2: '月收入10000+'
+      },
+      {
+        text1: '更多特权',
+        text2: '（开发中）'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
